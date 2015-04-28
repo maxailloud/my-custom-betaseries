f7b0a0dce7d224cf6e6dfaa00561d8d8e5f69495
Maxime AILLOUD <maxime.ailloud@gmail.com>
2015-04-28 13:11:33 +0200

    Display nicer image and fix t411 url

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
             var showActions = show.getElementsByClassName('actions');
             showActions[0].innerHTML += '<div class="fastforward" style="right: 87px;">' +
                 '<a href="https://www.t411.io/torrents/search/?search=' + showName + '&order=added&type=desc" target="_blank">' +
-                '<img src="/images/site/search.png" alt="Rechercher la série ' + showName + ' sur Torrent411" title="Rechercher la série ' + showName + ' sur Torrent411">' +
+                '<img src="https://www.t411.io/images/default.png" alt="Rechercher la série ' + showName + ' sur Torrent411" title="Rechercher la série ' + showName + ' sur Torrent411" class="t411-icon">' +
                 '</a></div>';
         }
     }
