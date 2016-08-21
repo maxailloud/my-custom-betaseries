8499398d6a5d21321e34b62c8d0401bfc2ba8a8d
Maxime Ailloud <maxime.ailloud@gmail.com>
2016-08-21 14:55:14 +0100

    Update according to new betaseries design

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 (function() {
     "use strict";
 
-    var shows = document.getElementsByClassName('show');
+    var shows = document.getElementsByClassName('showItem');
 
     for(var showNumber = 0; showNumber < shows.length; showNumber++) {
-        var show     = shows[showNumber];
+        var show = shows[showNumber];
+        
         if (!show.classList.contains('archive')) {
             var showName = show.getElementsByClassName('blue')[0].innerHTML;
             var showActions = show.getElementsByClassName('actions');
-            showActions[0].innerHTML += '<div class="fastforward" style="right: 87px;">' +
-                '<a href="https://www.t411.ch/torrents/search/?search=' + showName + '&order=added&type=desc" target="_blank">' +
+            var newAction = document.createElement("div");
+            var placeholder = show.getElementsByClassName('placeholder');
+            
+            show.removeChild(placeholder[0]);
+            
+            newAction.classList.add('t441');
+            newAction.classList.add('actionButton');
+            newAction.innerHTML = 
+            '<a href="https://www.t411.ch/torrents/search/?search=' + showName + '&order=added&type=desc" target="_blank">' +
                 '<img src="https://www.t411.ch/images/default.png" alt="Rechercher la série ' + showName + ' sur Torrent411" title="Rechercher la série ' + showName + ' sur Torrent411" class="t411-icon">' +
-                '</a></div>';
+            '</a>';
+
+            showActions[0].insertBefore(newAction, showActions[0].firstChild);
         }
     }
 
