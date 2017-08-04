dbad33e45b3f8fbaf0f3fa52e4b5d8eee973c766
Maxime Ailloud <maxime.ailloud@gmail.com>
2017-08-04 18:49:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
 (function() {
     "use strict";
-
-    var shows = document.getElementsByClassName('showItem');
-
-    for(var showNumber = 0; showNumber < shows.length; showNumber++) {
-        var show = shows[showNumber];
-        
-        if (!show.classList.contains('archive')) {
-            var showName = show.getElementsByClassName('blue')[0].innerHTML;
-            var showActions = show.getElementsByClassName('actions');
-            var newAction = document.createElement("div");
-            var placeholder = show.getElementsByClassName('placeholder');
-            
-            show.removeChild(placeholder[0]);
-            
-            newAction.classList.add('t441');
-            newAction.classList.add('actionButton');
-            newAction.innerHTML = 
-            '<a href="https://www.t411.al/torrents/search/?search=' + showName + '&order=added&type=desc" target="_blank">' +
-                '<img src="https://www.t411.al/images/default.png" alt="Rechercher la série ' + showName + ' sur Torrent411" title="Rechercher la série ' + showName + ' sur Torrent411" class="t411-icon">' +
-            '</a>';
-
-            showActions[0].insertBefore(newAction, showActions[0].firstChild);
-        }
-    }
-
     document.getElementById('ongoing').click();
 })();
